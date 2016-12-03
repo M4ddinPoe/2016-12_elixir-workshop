7f039faa1bdaa10a27765e868faf901909e8525a
Martin Pöpel <martin.poepel@5minds.de>
2016-12-03 14:39:21 +0100

    Task2: Add Page show action

---
+++ web/controllers/page_controller.ex
@@ -0,0 +1,16 @@
+defmodule Wiki.PageController do
+  use Wiki.Web, :controller 
+  
+  def index(conn, _params) do
+
+    pages = Repo.all(Wiki.Page)
+
+    render conn, "index.html", pages: pages
+  end
+
+  def show(conn, %{"id" => id}) do
+
+    page = Repo.get_by!(Wiki.Page, title: id)
+    render conn, "show.html", page: page
+  end
+end
